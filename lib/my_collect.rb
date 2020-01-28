2a14ea95cb49cac714c37c6aadf25d69a168cd5c
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-28 13:46:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,14 @@
 def my_collect(collection)
   if block_given?
     i = 0 
+    new_collection = []
     while i < collection.length 
-    yield collection[i]
+    new_collection << yield collection[i]
+    i = i + 1
+  end
   else
     puts "Sorry No block given"
   end
+  new_collection 
 end
 
