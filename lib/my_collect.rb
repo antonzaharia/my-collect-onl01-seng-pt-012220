557bc7b8a0e00a1d31b7ca5d75d44b8613a39b50
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-28 13:54:11 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,10 @@
-def my_collect(collection)
-
-    i = 0 
-    new_collection = []
-    while i < collection.length 
-    new_collection << yield collection[i]
-    i = i + 1
-    end
-    new_collection
-
-   
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
 
